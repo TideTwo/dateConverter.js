747afd17e22bbb28b3487ad776757aa63f4c1ca2
TideTwo <thedarkvibe1@gmail.com.com>
2023-07-10 21:50:19 +0200

    added helper method for unit multipliers

--- dateConvert.js
+++ dateConvert.js
@@ -1,13 +1,21 @@
 function dateConvert(date) {
-  const weeks = date / (1000 * 60 * 60 * 24 * 7);
-  const weeksMS = Math.floor(weeks) * (1000 * 60 * 60 * 24 * 7);
-  const days = (date - weeksMS) / (1000 * 60 * 60 * 24);
-  const daysMS = Math.floor(days) * (1000 * 60 * 60 * 24);
-  const hours = (date - weeksMS - daysMS) / (1000 * 60 * 60);
-  const hoursMS = Math.floor(hours) * (1000 * 60 * 60);
-  const minutes = (date - weeksMS - daysMS - hoursMS) / (1000 * 60);
-  const minutesMS = Math.floor(minutes) * 1000 * 60;
-  const seconds = (date - weeksMS - daysMS - hoursMS - minutesMS) / 1000;
+  function multiplierCalc(multipliers, amount) {
+    let calculated = 1;
+    for (let index = 0; index < amount; index++) {
+      calculated *= multipliers[index];
+    }
+    return calculated;
+  }
+  const multipliers = [1000, 60, 60, 24, 7];
+  const weeks = date / multiplierCalc(multipliers, 5);
+  const weeksMS = Math.floor(weeks) * multiplierCalc(multipliers, 5);
+  const days = (date - weeksMS) / multiplierCalc(multipliers, 4);
+  const daysMS = Math.floor(days) * multiplierCalc(multipliers, 4);
+  const hours = (date - weeksMS - daysMS) / multiplierCalc(multipliers, 3);
+  const hoursMS = Math.floor(hours) * multiplierCalc(multipliers, 3);
+  const minutes = (date - weeksMS - daysMS - hoursMS) / multiplierCalc(multipliers, 2);
+  const minutesMS = Math.floor(minutes) * multiplierCalc(multipliers, 2);
+  const seconds = (date - weeksMS - daysMS - hoursMS - minutesMS) / multiplierCalc(multipliers, 1);
   return [Math.floor(weeks), Math.floor(days), Math.floor(hours), Math.floor(minutes), Math.floor(seconds)];
 }
 
