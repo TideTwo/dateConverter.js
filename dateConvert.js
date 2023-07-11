2b74e8b03ff0860dfbc5b4aae080e53c3a922fcc
TideTwo <thedarkvibe1@gmail.com.com>
2023-07-11 10:28:33 +0200

    MS grouped

--- dateConvert.js
+++ dateConvert.js
@@ -8,16 +8,16 @@ function dateConvert(date) {
   }
   const multipliers = [1000, 60, 60, 24, 7];
   let units = [0, 0, 0, 0, 0];
-  let unitsMS = [0, 0, 0, 0, 0];
+  let accumulatedMS = 0;
   units[0] = date / mulCalc(multipliers, 5);
-  unitsMS[0] = Math.floor(units[0]) * mulCalc(multipliers, 5);
-  units[1] = (date - unitsMS[0]) / mulCalc(multipliers, 4);
-  unitsMS[1] = Math.floor(units[1]) * mulCalc(multipliers, 4);
-  units[2] = (date - unitsMS[0] - unitsMS[1]) / mulCalc(multipliers, 3);
-  unitsMS[2] = Math.floor(units[2]) * mulCalc(multipliers, 3);
-  units[3] = (date - unitsMS[0] - unitsMS[1] - unitsMS[2]) / mulCalc(multipliers, 2);
-  unitsMS[3] = Math.floor(units[3]) * mulCalc(multipliers, 2);
-  units[4] = (date - unitsMS[0] - unitsMS[1] - unitsMS[2] - unitsMS[3]) / mulCalc(multipliers, 1);
+  accumulatedMS += Math.floor(units[0]) * mulCalc(multipliers, 5);
+  units[1] = (date - accumulatedMS) / mulCalc(multipliers, 4);
+  accumulatedMS += Math.floor(units[1]) * mulCalc(multipliers, 4);
+  units[2] = (date - accumulatedMS) / mulCalc(multipliers, 3);
+  accumulatedMS += Math.floor(units[2]) * mulCalc(multipliers, 3);
+  units[3] = (date - accumulatedMS) / mulCalc(multipliers, 2);
+  accumulatedMS += Math.floor(units[3]) * mulCalc(multipliers, 2);
+  units[4] = (date - accumulatedMS) / mulCalc(multipliers, 1);
   return units.map((unit) => {
     return Math.floor(unit);
   });
